c00d559715cc06e09c588cd6b8e3957f505f3726
VianneyBRZ <vianney.broyez@gmail.com>
2020-10-22 16:33:19 +0200

    Class Contact

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -2,9 +2,36 @@
 
 
 namespace App\Entity;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
 
-
+/**
+ * Class Contact
+ * @ORM\Entity
+ * @ORM\Table(name=Contact)
+ */
 class Contact
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id_contact", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $idContact;
+
+    /**
+     * @ORM\Column(name="id_voyageur", type="integer")
+     */
+    private $idVoyageur;
+
+    /**
+     * @ORM\Column (name="methode", type="text")
+     */
+    private $methode;
+
+    /**
+     * @ORM\Column (name="valeur", type="text")
+     */
+    private $valeur;
 
 }
